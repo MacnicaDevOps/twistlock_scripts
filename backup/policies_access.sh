f81317d4850037fe0e9281ea9a68e725916500ef
Minehiko Nohara <minepicco@gmail.com>
2020-04-21 15:02:41 +0900

    Update policies_access.sh

--- backup/policies_access.sh
+++ backup/policies_access.sh
@@ -2,6 +2,6 @@
 
 source conf.txt
 
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/docker" -o $dir"/policies_docker.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/secrets" -o $dir"/policies_secrets.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/kubernetes-audit" -o $dir"/policies_kubernetes-audit.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/docker" -o $dir"/policies_docker.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/secrets" -o $dir"/policies_secrets.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/kubernetes-audit" -o $dir"/policies_kubernetes-audit.yaml"
