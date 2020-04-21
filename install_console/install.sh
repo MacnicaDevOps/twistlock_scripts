01102aa66971287d4c4e118033ffee4cd5b17f73
Minehiko Nohara <minepicco@gmail.com>
2020-04-21 13:23:50 +0900

    Update install.sh

--- install_console/install.sh
+++ install_console/install.sh
@@ -3,7 +3,9 @@
 source config.txt
 
 wget $path
-tar zxvf $file
-$platform"/twistcli console export kubernetes --service-type LoadBalancer --registry-token" $token
+mkdir twistlock
+tar zxvf $file -o twistlock
+"twistlock/"$platform"/twistcli console export kubernetes --service-type LoadBalancer --registry-token" $token
 kubectl apply -f twistlock_console.yaml | tee twistlock_console_install_result.log
 rm -rf $file
+rm -rf twistlock/
