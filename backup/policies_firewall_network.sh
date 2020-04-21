bd3d9c141c1d5b9843a5c7e770aa61b6435c00ad
Minehiko Nohara <minepicco@gmail.com>
2020-04-21 15:03:57 +0900

    Update policies_firewall_network.sh

--- backup/policies_firewall_network.sh
+++ backup/policies_firewall_network.sh
@@ -2,6 +2,6 @@
 
 source config.txt
 
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/network/container" -o $dir"/policies_firewall_network_container.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/network/host" -o $dir"/policies_firewall_network_host.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/network/entities" -o $dir"/policies_firewall_network_entities.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/network/container" -o $dir"/policies_firewall_network_container.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/network/host" -o $dir"/policies_firewall_network_host.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/network/entities" -o $dir"/policies_firewall_network_entities.yaml"
