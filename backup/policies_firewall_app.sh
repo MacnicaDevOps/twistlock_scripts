f78ea592f126701204c60f603d2e54ed45d7538d
Minehiko Nohara <minepicco@gmail.com>
2020-04-21 15:03:35 +0900

    Update policies_firewall_app.sh

--- backup/policies_firewall_app.sh
+++ backup/policies_firewall_app.sh
@@ -2,7 +2,7 @@
 
 source conf.txt
 
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/container" -o $dir"/policies_firewall_app_container.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/host" -o $dir"/policies_firewall_app_host.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/app-embedded" -o $dir"/policies_firewall_app_app-embedded.yaml"
-curl -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/serverless" -o $dir"/policies_firewall_app_serverless.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/container" -o $dir"/policies_firewall_app_container.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/host" -o $dir"/policies_firewall_app_host.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/app-embedded" -o $dir"/policies_firewall_app_app-embedded.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/firewall/app/serverless" -o $dir"/policies_firewall_app_serverless.yaml"
