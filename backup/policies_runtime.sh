b36b9821de5fe149785d9c4b80f2acff45c1e837
Minehiko Nohara <minepicco@gmail.com>
2020-04-22 10:12:00 +0900

    Update policies_runtime.sh

--- backup/policies_runtime.sh
+++ backup/policies_runtime.sh
@@ -2,8 +2,8 @@
 
 source conf.txt
 
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/container" -o $dir"/policies_runtime_container.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/host" -o $dir"/policies_runtime_host.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/serverless" -o $dir"/policies_runtime_serverless.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/app-embedded" -o $dir"/policies_runtime_app-embedded.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/custom-rules" -o $dir"/policies_runtime_custome-rules.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/container" -o $dir"/policies_runtime_container.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/host" -o $dir"/policies_runtime_host.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/serverless" -o $dir"/policies_runtime_serverless.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/app-embedded" -o $dir"/policies_runtime_app-embedded.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/runtime/custom-rules" -o $dir"/policies_runtime_custom-rules.json"
