e8a74ee23f71399ecb668098b8062feb2c8524a3
Minehiko Nohara <minepicco@gmail.com>
2020-04-22 10:13:20 +0900

    Update policies_compliance.sh

--- backup/policies_compliance.sh
+++ backup/policies_compliance.sh
@@ -2,9 +2,9 @@
 
 source conf.txt
 
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/compliance/container" -o $dir"/policies_compliance_container.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/compliance/host" -o $dir"/policies_compliance_host.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/compliance/serverless" -o $dir"/policies_compliance_serverless.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/trust" -o $dir"/policies_trust.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/custom-compliance" -o $dir"/custom-compliance.yaml"
-curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/cloud-platforms" -o $dir"/policies_cloud-platforms.yaml"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/compliance/container" -o $dir"/policies_compliance_container.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/compliance/host" -o $dir"/policies_compliance_host.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/compliance/serverless" -o $dir"/policies_compliance_serverless.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/trust" -o $dir"/policies_trust.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/custom-compliance" -o $dir"/custom-compliance.json"
+curl -k -u $cred -H "Content-Type:application/json" -X GET $source"/api/v1/policies/cloud-platforms" -o $dir"/policies_cloud-platforms.json"
